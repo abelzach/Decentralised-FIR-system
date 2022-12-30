713882339175d6297d83d2b97069f6d5eadc7351
abelzach <abelsimonz@gmail.com>
2022-12-30 22:17:33 +0530

    metamask connect and address retreival

--- packages/next-app/pages/index.tsx
+++ packages/next-app/pages/index.tsx
@@ -3,8 +3,44 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from 'next/link'
+import { ethers } from "ethers";
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+
+  const [walletAddress,setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    //console.log("Inside Function requestAccount");
+
+    if(window.ethereum) {
+      //console.log("Detected Metamsk");
+      try{
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log(accounts);
+        setWalletAddress(accounts[0]);
+      }
+      catch(error) {
+        console.log("Error connecting");
+      }
+    }
+    else {
+      alert("Please install metamask");
+    }
+  }
+
+  async function connectWallet() {
+    if(typeof window.ethereum !== 'undefined')
+    {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -28,9 +64,13 @@ const Home: NextPage = () => {
             justifyContent: "center"
           }}
         >
-          <ConnectButton accountStatus="avatar" label="Sign in"/>
+        <button onClick={requestAccount} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          Connect
+        </button>
+        
+        
         </div>
-
+        <h3>Address : {walletAddress}</h3>
         <div className={styles.grid}>
             <h2>So guys this can be the main page from where we can direct to the other pages</h2>
         </div>
