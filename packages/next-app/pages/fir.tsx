1ef13ba0aa8099d216bd334a465b9ce69f4bbda6
abelzach <abelsimonz@gmail.com>
2022-12-30 22:45:15 +0530

    FIR form layout done

--- packages/next-app/pages/fir.tsx
+++ packages/next-app/pages/fir.tsx
@@ -11,26 +11,6 @@ export default function FIR() {
                 <Link href="/">Back to home</Link>
             </h3>
             <br/>
-            {/* <form action="/" method="post">
-            <div>
-            <div className="mb-6">
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Large input</label>
-                <input type="text" id="large-input" className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-            <div className="mb-6">
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Base input</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Small input</label>
-                <input type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-            <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-            </div>
-                <br/><br/>
-                <button type="submit">Submit</button>
-            </form> */}
             <form className="w-full max-w-lg">
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
@@ -50,7 +30,7 @@ export default function FIR() {
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                         FIR Description
                     </label>
                     <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
