0d0cf123edc3bd2571bc82ea5e3b078fdf639517
Diya-James <diyaperumthottam@gmail.com>
2023-01-02 21:01:43 +0530

    Evidence cards through json file

--- packages/next-app/pages/evidenceCards.tsx
+++ packages/next-app/pages/evidenceCards.tsx
@@ -1,5 +1,27 @@
 import { Center } from '@chakra-ui/react';
 import Link from 'next/link';
+import { Card, Stack, StackDivider, Box, Button, Heading, Text, CardHeader, CardBody, CardFooter, SimpleGrid } from '@chakra-ui/react'
+import {Evidences} from './evidencedata'
+
+
+
+interface Evidence {
+    "fir_no" : number,
+    "dor" : string,
+    "police_station" : string,
+    "name_complainant" : string,
+    "name_accused" : string,
+    "applicant_detail_name" : string,
+    "applicant_detail_parentage" : string,
+    "applicant_detail_address" : string,
+    "applicant_detail_contact_no" : number,
+    "applicant_relationship_accussed" : string,
+}
+
+interface Evidences_Props extends Array<Evidence>{};
+
+var Evi: Evidences_Props = Evidences;
+console.log(Evi);
 
 export default function evidenceCards() {
     return (
@@ -12,128 +34,139 @@ export default function evidenceCards() {
             </h3>
         </center>
 
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5" >
-            <div className="w-full rounded-lg shadow-md lg:max-w-sm">
-            <img
-                className="object-cover w-full h-48"
-                src="https://cdn.pixabay.com/photo/2022/08/18/09/20/houses-7394390__340.jpg"
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                    React Tailwind Card with Image
-                </h4>
-                <p className="mb-2 leading-normal">
-                    react tailwind css card with image It is a long established
-                    fact that a reader will be distracted by the readable
-                    content.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
+        
 
-        <div className="w-full rounded-lg shadow-md lg:max-w-sm">
-            <img
-                className="object-cover w-full h-48"
-                src="https://cdn.pixabay.com/photo/2022/08/18/09/20/houses-7394390__340.jpg"
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                    React Tailwind Card with Image
-                </h4>
-                <p className="mb-2 leading-normal">
-                    react tailwind css card with image It is a long established
-                    fact that a reader will be distracted by the readable
-                    content.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
+        <SimpleGrid spacing={10} templateColumns='repeat(auto-fill, minmax(300px, 4fr))'>
 
-        <div className="w-full rounded-lg shadow-md lg:max-w-sm">
-            <img
-                className="object-cover w-full h-48"
-                src="https://cdn.pixabay.com/photo/2022/08/18/09/20/houses-7394390__340.jpg"
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                    React Tailwind Card with Image
-                </h4>
-                <p className="mb-2 leading-normal">
-                    react tailwind css card with image It is a long established
-                    fact that a reader will be distracted by the readable
-                    content.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
+        {Evi.map(evi => <Card> 
+                            <CardHeader>
+                                <Heading size='md'>FIR {evi.fir_no}</Heading>
+                            </CardHeader>
 
-        <div className="w-full rounded-lg shadow-md lg:max-w-sm">
-            <img
-                className="object-cover w-full h-48"
-                src="https://cdn.pixabay.com/photo/2022/08/18/09/20/houses-7394390__340.jpg"
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                    React Tailwind Card with Image
-                </h4>
-                <p className="mb-2 leading-normal">
-                    react tailwind css card with image It is a long established
-                    fact that a reader will be distracted by the readable
-                    content.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
+                            <Stack divider={<StackDivider />} spacing='4'>
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Date of Registration
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.dor}
+                                </Text>
+                            </Box>
 
-        <div className="w-full rounded-lg shadow-md lg:max-w-sm">
-            <img
-                className="object-cover w-full h-48"
-                src="https://cdn.pixabay.com/photo/2022/08/18/09/20/houses-7394390__340.jpg"
-                alt="image"
-            />
-            <div className="p-4">
-                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                    React Tailwind Card with Image
-                </h4>
-                <p className="mb-2 leading-normal">
-                    react tailwind css card with image It is a long established
-                    fact that a reader will be distracted by the readable
-                    content.
-                </p>
-                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                    Read more
-                </button>
-            </div>
-        </div>
 
-        </div>
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Police Station
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.police_station}
+                                </Text>
+                            </Box>
 
-        <style jsx>{`
-            h1{
-                font-size: 70px;
-                font-weight: 800;
-            }
-            p {
-            color: blue;
-            }
-            h3{
-                font-size: 30px;
-                font-weight: 400;
-                color: #0070f3;
-            }
-        `}</style>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Name of the Complainant
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.name_complainant}
+                                </Text>
+                            </Box>
+
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Person Accussed
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.name_accused}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Person Accused
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.name_accused}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Applicant Detail
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.applicant_detail_name}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Applicant's Parentage
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.applicant_detail_parentage}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Applicant's Address
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.applicant_detail_address}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Applicant's Contact detail
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.applicant_detail_contact_no}
+                                </Text>
+                            </Box>
+
+                            <Box>
+                                <Heading size='xs' textTransform='uppercase'>
+                                Applicant's Relationship with Accussed
+                                </Heading>
+                                <Text pt='2' fontSize='sm'>
+                                {evi.applicant_relationship_accussed}
+                                </Text>
+                            </Box>
+
+
+
+
+                            </Stack>
+
+                            
+                        </Card>
+                                
+                )
+        }
+
+
+            
+            </SimpleGrid>
+            <style jsx>{`
+                h1{
+                    font-size: 70px;
+                    font-weight: 800;
+                }
+                p {
+                color: blue;
+                }
+                h3{
+                    font-size: 30px;
+                    font-weight: 400;
+                    color: #0070f3;
+                }
+            `}</style>
+
+        
         </div>
         </>
     )
---
+++ packages/next-app/pages/evidencedata.js
@@ -0,0 +1,45 @@
+export let Evidences = [
+{ 
+    "fir_no" : 1,
+    "dor" : "12/12/21",
+    "police_station" : "mundakayam",
+    "name_complainant" : "abel",
+    "name_accused" : "abel",
+    "applicant_detail_name" : "abel",
+    "applicant_detail_parentage" : "abel",
+    "applicant_detail_address" : "abel",
+    "applicant_detail_contact_no" : 72817287,
+    "applicant_relationship_accussed" : "abel",
+
+},
+{ 
+    "fir_no" : 2,
+    "dor" : "12/12/21",
+    "police_station" : "mundakayam",
+    "name_complainant" : "abel",
+    "name_accused" : "abel",
+    "applicant_detail_name" : "abel",
+    "applicant_detail_parentage" : "abel",
+    "applicant_detail_address" : "abel",
+    "applicant_detail_contact_no" : 72817287,
+    "applicant_relationship_accussed" : "abel",
+
+},
+{ 
+    "fir_no" : 3,
+    "dor" : "12/12/21",
+    "police_station" : "mundakayam",
+    "name_complainant" : "abel",
+    "name_accused" : "abel",
+    "applicant_detail_name" : "abel",
+    "applicant_detail_parentage" : "abel",
+    "applicant_detail_address" : "abel",
+    "applicant_detail_contact_no" : 72817287,
+    "applicant_relationship_accussed" : "abel",
+
+},
+
+
+
+
+]
