9f5697d5e89e4c6ce0a84ed6470b02d296a0039f
unknown <abijithmv01@gmail.com>
2023-01-04 23:58:37 +0530

    Fixed fir layout

--- packages/next-app/pages/fir.tsx
+++ packages/next-app/pages/fir.tsx
@@ -1,142 +1,134 @@
-import { Center } from '@chakra-ui/react';
-import Link from 'next/link';
-
 export default function FIR() {
     return (
-        <>
-        <div className='px-2 font-inter'>
-        <nav className="sm:px-4 py-2.5 opacity-100">
-        <div className="container flex px-16 flex-wrap items-center justify-between mx-auto">
-            <a href="/" className="flex items-center">
-                <span className="self-center text-xl font-bold whitespace-nowrap dark:text-black">DFIR</span>
-            </a>
-        </div>
-        </nav>
         
-        <div className='flex flex-wrap h-screen flex-row justify-between'>
-        
-        <div className=' w-3.5/12 bg-gradient-to-l from-gray-700 via-gray-900 to-black'>
-            <div className='flex flex-col px-20 gap-6'>
-            <center>
-            <p className='text-3xl font-bold mt-16 leading-tight text-white'>
-            Filed by
+    <div className='font-inter'>    
+    <nav className="sm:px-4 py-2.5 opacity-100">
+      <div className="container flex px-16 flex-wrap items-center justify-between mx-auto">
+        <a href="/" className="flex items-center">
+            <span className="self-center text-xl font-bold whitespace-nowrap dark:text-black">DFIR</span>
+        </a>
+      </div>
+    </nav>
+    <div className='flex flex-wrap h-screen flex-row justify-between'>
+      <div className=' w-2/12 bg-[#131313]'>
+        <div className='flex flex-col items-center'>
+            <h1 className='mt-24 text-2xl font-extralight text-gray-100'>
+                Filed by
+            </h1>
+            <p className='mt-4 text-base font-normal text-gray-200'>
+                Kakkanad Police Station
             </p>
-            <svg className="w-6 h-6" fill="white" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-            <p className='text-2xl font-bold mt-1 leading-tight text-white'>
-            Kakkanad Police Station
-            </p>
-            </center>
-            </div>
         </div>
-        
-        <div className='w-8/12'>
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
-        </svg>
-        <h1>FIR Registration</h1>
-            <p className='text-2xl font-bold mt-1 leading-tight text-slate-600'>Fill in the details regarding the report</p>
-            <br/>
-            <form className="w-full max-w-lg">
-                <div className="flex flex-wrap -mx-3 mb-6">
+      </div>
+      <div className='w-10/12'>
+        <div className='ml-10'>
+                <a href="/">
+                <div className='flex flex-row gap-2'>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
+                </svg>
+                <p>
+                    Home
+                </p>
+                </div>
+                </a>
+        <h1 className='text-4xl font-bold mt-4'>FIR Registration</h1>
+             <p className='text-xl font-normal mt-2 text-slate-500'>Fill in the details regarding the report</p>
+             <br/>
+             <form className="w-ful">
+              <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
                         FIR ID
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Name of Complaintent
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                        NAME OF COMPLAINTENT
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Name of Accused
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                        NAME OF ACCUSED
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Applicant Detailed name
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                        APPLICANT DETAILED NAME
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Applicant Detailed Parentage
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                    APPLICANT DETAILED PARENTAGE
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Applicant address
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                    APPLICANT ADDRESS
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
-                    </div>
-                </div><div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Applicant contact number
-                    </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="number"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                        Applicant Relationship with accused
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                    APPLICANT CONTACT NUMBER
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
+                    APPLICANT RELATIONSHIP WITH ACCUSED
+                    </label>
+                    <input className="appearance-none w-11/12 bg-gray-200 text-gray-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ID" type="text"/>
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2">
                         FIR Description
                     </label>
-                    <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="Desc"></textarea>
+                    <textarea className=" no-resize appearance-none block w-11/12 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="Desc"></textarea>
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                    <label className="block uppercase tracking-wide text-gray-900 text-base font-normal mb-2" >
                         IPC SECTIONS
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ipc" type="text"/>
+                    <input className="appearance-none block w-11/12 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="ipc" type="text"/>
                     </div>
                 </div>
 
-                <div className="md:flex md:items-center">
-                    <div className="md:w-1/3">
-                    <button className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                <div className="flex justify-end md:flex md:items-center">
+                    <div className="mr-24">
+                    <button className="shadow bg-gradient-to-br from-teal-200 via-teal-400 to-teal-800 w-48 h-12 mt-4 text-slate-100focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
                         Submit
                     </button>
                     </div>
-                    <div className="md:w-2/3"></div>
+                    <div className=""></div>
                 </div>
                 </form>
+            </div>
         </div>
-        </div>
-        </div>
-        <style jsx>{`
-            h1{
-                font-size: 50px;
-                font-weight: 600;
-            }
-           
-        `}</style>
-       
-        </>
+    </div>
+    </div>
     )
 
 }
